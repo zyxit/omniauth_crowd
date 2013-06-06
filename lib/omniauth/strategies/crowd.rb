2d74ff43eea0f3e6fa7536aa4ca7f74980bf5660
Kostya Marchenko <kostya@atlassian.com>
2013-06-06 15:56:09 +1000

    Reverting

--- lib/omniauth/strategies/crowd.rb
+++ lib/omniauth/strategies/crowd.rb
@@ -34,9 +34,7 @@ module OmniAuth
         creds = session.delete 'omniauth.crowd'
         return fail!(:no_credentials) unless creds
         validator = CrowdValidator.new(@configuration, creds['username'], creds['password'])
-        @user_info = validator.user_info
-
-        puts "User info: #{@user_info}"
+        @user_info = validator.user_info        
         
         return fail!(:invalid_credentials) if @user_info.nil? || @user_info.empty?
     
